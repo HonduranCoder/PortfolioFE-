360be373cd6c8bbf22faa10bf43f63244bd5f61f
Estefani <stef45689@gmail.com>
2022-05-04 08:32:25 -0500

    profilebox

--- src/components/ProfileBox/ProfileBox.jsx
+++ src/components/ProfileBox/ProfileBox.jsx
@@ -4,7 +4,7 @@ import {
   Center,
   Heading,
   Text,
-  Image, //need to get next/image working
+  Image, //need to get next/image
   Stack,
   Avatar,
   useColorModeValue,
@@ -32,9 +32,69 @@ export default function blogPostWithImage() {
         >
           <Image
             src={
-              'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+              'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80'
             }
-            layout={'fill'}
+          />
+        </Box>
+        <Stack style={{ margin: 0, padding: 10 }}>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}
+          >
+            The Engineer
+          </Text>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize={'2xl'}
+            fontFamily={'body'}
+          >
+            <Text> - </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            I exist and I write code. I am a Full-Stack JavaScript Engineer who
+            loves to build end-to-end products including product concept,
+            planning, design, development, testing, and deployment.
+          </Text>
+        </Stack>
+        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+          <Avatar
+            src={
+              'https://images.unsplash.com/photo-1507290439931-a861b5a38200?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80'
+            }
+            alt={'Stef'}
+          />
+          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+            <Text fontWeight={600}>Software Engineer</Text>
+            {/* <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text> */}
+          </Stack>
+        </Stack>
+      </Box>
+      <Box
+        maxW={'445px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        p={6}
+        overflow={'hidden'}
+      >
+        <Box
+          h={'210px'}
+          bg={'gray.100'}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={'relative'}
+        >
+          <Image
+            h={'210px'}
+            w={'399px'}
+            src={
+              'https://images.unsplash.com/photo-1587586291914-bdbe84ec1416?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+            }
           />
         </Box>
         <Stack>
@@ -45,30 +105,153 @@ export default function blogPostWithImage() {
             fontSize={'sm'}
             letterSpacing={1.1}
           >
-            Blog
+            The Idealist
           </Text>
           <Heading
             color={useColorModeValue('gray.700', 'white')}
             fontSize={'2xl'}
             fontFamily={'body'}
           >
-            Boost your conversion rate
+            <Text> - </Text>
           </Heading>
           <Text color={'gray.500'}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
+            I value diverse thinking in a multicultural workplace. I have worked
+            on multiple development teams that all had members with different
+            backgrounds. This experience has made me grow as a person and a
+            developer.
           </Text>
         </Stack>
         <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
           <Avatar
-            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
-            alt={'Author'}
+            src={
+              'https://images.unsplash.com/photo-1587136527307-f53f514267d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1624&q=80'
+            }
+            alt={'Stef'}
           />
           <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
+            <Text fontWeight={600}>Diversity</Text>
+          </Stack>
+        </Stack>
+      </Box>
+      <Box
+        maxW={'445px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        p={6}
+        overflow={'hidden'}
+      >
+        <Box
+          h={'210px'}
+          bg={'gray.100'}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={'relative'}
+        >
+          <Image
+            h={'210px'}
+            w={'399px'}
+            src={
+              'https://images.unsplash.com/photo-1590598687530-581406de2262?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+            }
+          />
+        </Box>
+        <Stack>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}
+          >
+            The Bibliophile
+          </Text>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize={'2xl'}
+            fontFamily={'body'}
+          >
+            <Text> - </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            On a personal note, I love to read. I love to read everything, from
+            nonfiction & fiction to comic books & manga to poetry & short
+            stories. Our dog Mia is named after the main character in the
+            Nevernight Trilogy.
+          </Text>
+        </Stack>
+        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+          <Avatar
+            src={
+              'https://images.unsplash.com/photo-1575709527142-a93ed587bb83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80'
+            }
+            alt={'Stef'}
+          />
+          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+            <Text fontWeight={600}>A Note</Text>
+          </Stack>
+        </Stack>
+      </Box>
+      <Box
+        maxW={'445px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        p={6}
+        overflow={'hidden'}
+      >
+        <Box
+          h={'210px'}
+          bg={'gray.100'}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={'relative'}
+        >
+          <Image
+            h={'210px'}
+            w={'399px'}
+            src={
+              'https://images.unsplash.com/photo-1526778548025-fa2f459cd5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2300&q=80'
+            }
+          />
+        </Box>
+        <Stack>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}
+          >
+            The Wanderer
+          </Text>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize={'2xl'}
+            fontFamily={'body'}
+          >
+            <Text> - </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            I love to travel. Seeing the world, meeting new people, eating new
+            food is the happiness in my life. My favorite place is Iceland and
+            my favorite place there is the ice caves. MY favorite food there was
+            the Skyr, which is much sweeter there.
+          </Text>
+        </Stack>
+        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+          <Avatar
+            src={
+              'https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80'
+            }
+            alt={'Stef'}
+          />
+          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+            <Text fontWeight={600}>Travel</Text>
           </Stack>
         </Stack>
       </Box>
